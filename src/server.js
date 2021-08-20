c1962d1863a13c05fb0f8fb1ebe08e4da5bbbaf3
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-20 12:21:14 +0200

    routers

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
+import mediaRouters from "./service/media/index.js";
 
 const server = express();
 const port = process.env.PORT;
 
 server.use(express.json());
 server.use(cors());
+
+server.use("/media", mediaRouters);
 
 console.table(listEndpoints, server);
 server.listen(port, () => {
--- src/service/media/index.js
+++ src/service/media/index.js
@@ -2,4 +2,35 @@ import { Router } from "express";
 
 const mediaRouters = Router();
 
+mediaRouters.get("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+mediaRouters.get("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+mediaRouters.post("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+mediaRouters.put("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+mediaRouters.delete("/:id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default mediaRouters;
