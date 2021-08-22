e75022e08ff742f834fb74c9f4b786855735b33e
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-22 09:17:52 +0200

    .

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 import mediaRouters from "./service/media/index.js";
+import {
+  notFoundHandler,
+  forbidenHandler,
+  genericErrorHandler,
+  badRequestHandler,
+} from "./errorHandlers.js";
 
 const server = express();
 const port = process.env.PORT;
@@ -10,6 +16,11 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/media", mediaRouters);
+
+server.use(notFoundHandler);
+server.use(forbidenHandler);
+server.use(badRequestHandler);
+server.use(genericErrorHandler);
 
 console.table(listEndpoints(server));
 server.listen(port, () => {
